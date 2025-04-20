2cfd1aa8ec14cf03d198328f6d5bad3d237cf44b
jjeris <johans.justs.eris@gmail.com>
2025-04-20 18:59:32 +0300

    Add FE code

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,34 +1,15 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRouter from './router';
+import TitleBar from './components/titleBar/TitleBar';
 
-function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
+const App = () => {
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
-}
+    <Router>
+      <TitleBar />
+      <AppRouter />
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/renderer/src/components/TitleBar/TitleBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TitleBar.css';
+
+const TitleBar = () => {
+  return (
+    <div className="titlebar">
+      <span className="title">Blendio-ElectronJS</span>
+    </div>
+  );
+};
+
+export default TitleBar;
